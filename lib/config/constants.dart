e51a8ef01708881aae71e3fabbad9bc08960eb37
zuhdiarifazizi <azizi6506.zaa@gmail.com>
2026-06-03 14:25:03 +0700

    fix: correct Supabase URL in constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   AppConstants._();
 
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const String supabaseUrl = 'https://qeipqnzsqefebevgucgb.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlaXBxbnpzcWVmZWJldmd1Y2diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NjcxNzUsImV4cCI6MjA5NjA0MzE3NX0.dfCqaYNIds-_i4nWsoUpZsqDhqmLcRrE-2hdY7umobQ';
   static const String mlApiUrl = 'http://10.0.2.2:5000/api';
 }
