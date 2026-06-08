7236044b754ec29054c8136e8a0f6083137438f1
zuhdiarifazizi <azizi6506.zaa@gmail.com>
2026-06-08 14:54:27 +0700

    chore: update mlApiUrl to live Railway production API

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -3,5 +3,5 @@ class AppConstants {
 
   static const String supabaseUrl = 'https://qeipqnzsqefebevgucgb.supabase.co';
   static const String supabaseAnonKey = 'sb_publishable_qA0yqU3cv2x_W2XCBqwISA_pkzjU-FJ';
-  static const String mlApiUrl = 'http://192.168.100.20:5000';
+  static const String mlApiUrl = 'https://beapk1-production.up.railway.app';
 }
